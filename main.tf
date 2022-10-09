772ae64dbfe65a0aed5d667398b751aa79e28640
Rui Lopes <rgl@ruilopes.com>
2022-10-09 06:30:05 +0100

    upgrade to terraform 1.3.2

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # see https://github.com/hashicorp/terraform
 terraform {
-  required_version = "1.3.1"
+  required_version = "1.3.2"
   required_providers {
     # see https://github.com/hashicorp/terraform-provider-random
     # see https://registry.terraform.io/providers/hashicorp/random
