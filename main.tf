1c43f67bc37343581cea9033575d1859f6b39c03
Rui Lopes <rgl@ruilopes.com>
2022-10-17 18:23:11 +0100

    upgrade to hashicorp/helm provider 2.7.1

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ terraform {
     # see https://registry.terraform.io/providers/hashicorp/helm
     helm = {
       source  = "hashicorp/helm"
-      version = "2.7.0"
+      version = "2.7.1"
     }
     # see https://registry.terraform.io/providers/gavinbunney/kubectl
     # see https://github.com/gavinbunney/terraform-provider-kubectl
